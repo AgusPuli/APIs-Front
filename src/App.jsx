357eb48e916e90942efe2348340e4ead7f628ffe
AgusPuli <Agustinmpulido@gmail.com>
2025-10-17 18:38:41 -0300

    in icio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tech Store 🚀
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Tailwind 4 funcionando correctamente
+      </p>
+    </div>
+  );
+}
