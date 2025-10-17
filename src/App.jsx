2e39d0c1c01ee2cedb987f47514a3cd74696ad9e
AgusPuli <Agustinmpulido@gmail.com>
2025-10-17 19:24:31 -0300

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
